5ab13974dafeec736e2608c8b2426f262b740448
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-03-29 23:38:07 +0600

    Update HomePageOne.jsx

--- master/src/pages/HomePageOne.jsx
+++ master/src/pages/HomePageOne.jsx
@@ -2,7 +2,7 @@ import HeaderOne from "../components/HeaderOne";
 // import AboutOne from "../components/AboutOne";
 import BannerOne from "../components/BannerOne";
 // import BlogOne from "../components/BlogOne";
-// import BrandOne from "../components/BrandOne";
+import BrandOne from "../components/BrandOne";
 // import CertificateOne from "../components/CertificateOne";
 // import ChooseUsOne from "../components/ChooseUsOne";
 // import CounterOne from "../components/CounterOne";
@@ -26,7 +26,7 @@ const HomePageOne = () => {
       <BannerOne />
 
       {/* BrandOne */}
-      {/* <BrandOne /> */}
+      <BrandOne />
 
       {/* FeaturesOne */}
       {/* <FeaturesOne /> */}
