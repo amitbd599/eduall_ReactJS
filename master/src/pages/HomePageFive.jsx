b5106e738d792e1319229a1a7c297f46de6c2a07
Amit Biswas <67472157+amitbd599@users.noreply.github.com>
2025-04-17 20:22:39 +0600

    InstructorTwo add

--- master/src/pages/HomePageFive.jsx
+++ master/src/pages/HomePageFive.jsx
@@ -3,8 +3,10 @@ import CounterFour from "../components/CounterFour";
 import FeaturesThree from "../components/FeaturesThree";
 import GallerySectionTwo from "../components/GallerySectionTwo";
 import HeaderThree from "../components/HeaderThree";
+import InstructorTwo from "../components/InstructorTwo";
 import KidsCourses from "../components/KidsCourses";
 import MarqueeOne from "../components/MarqueeOne";
+import PopularTwo from "../components/PopularTwo";
 import Animation from "../helper/Animation";
 import Preloader from "../helper/Preloader";
 
@@ -37,6 +39,12 @@ const HomePageFive = () => {
 
       {/* GallerySectionTwo */}
       <GallerySectionTwo />
+
+      {/* InstructorTwo */}
+      <InstructorTwo />
+
+      {/* PopularTwo */}
+      <PopularTwo />
     </>
   );
 };
